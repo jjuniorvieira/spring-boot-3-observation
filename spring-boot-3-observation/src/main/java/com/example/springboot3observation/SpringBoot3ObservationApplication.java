ef8ac3bc0e35f802ccd28b9caf35f91ddacde253
J. Junior <junior.jaava@gmail.com>
2024-03-21 18:58:06 +0100

    first full commit

---
+++ spring-boot-3-observation/src/main/java/com/example/springboot3observation/SpringBoot3ObservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3observation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ObservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3ObservationApplication.class, args);
+	}
+
+}
